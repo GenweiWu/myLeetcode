6a22c70e6d677119a70d9e0076b9eb645d5744b3
Dave.Wu <wugenwei2010@163.com>
2020-03-18 18:34:27 +0800

    Create TreeNode.java

---
+++ 二叉树/TreeNode.java
@@ -0,0 +1,23 @@
+package com.njust.learn.tree;
+
+import lombok.Getter;
+import lombok.Setter;
+
+public class TreeNode
+{
+    @Getter
+    private int value;
+    
+    @Getter
+    @Setter
+    private TreeNode leftNode;
+    
+    @Getter
+    @Setter
+    private TreeNode rightNode;
+    
+    public TreeNode(int value)
+    {
+        this.value = value;
+    }
+}
